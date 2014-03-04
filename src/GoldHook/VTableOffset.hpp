7e234195db1674a7cc504db86e15d092e9af40a1
Elliott Linder <elliott.darfink@gmail.com>
2014-03-04 12:42:52 +0100

    Made the 'VTableOffset' function more compact

--- src/GoldHook/VTableOffset.hpp
+++ src/GoldHook/VTableOffset.hpp
@@ -7,22 +7,22 @@ namespace gm {
     template <class T, typename F>
     size_t VTableOffset(F function) {
         struct /* Anonymous */ {
-            virtual size_t Get1()  { return 0 * sizeof(uintptr_t); }
-            virtual size_t Get2()  { return 1 * sizeof(uintptr_t); }
-            virtual size_t Get3()  { return 2 * sizeof(uintptr_t); }
-            virtual size_t Get4()  { return 3 * sizeof(uintptr_t); }
-            virtual size_t Get5()  { return 4 * sizeof(uintptr_t); }
-            virtual size_t Get6()  { return 5 * sizeof(uintptr_t); }
-            virtual size_t Get7()  { return 6 * sizeof(uintptr_t); }
-            virtual size_t Get8()  { return 7 * sizeof(uintptr_t); }
-            virtual size_t Get9()  { return 8 * sizeof(uintptr_t); }
-            virtual size_t Get10() { return 9 * sizeof(uintptr_t); }
+            virtual size_t Get1()  { return 0; }
+            virtual size_t Get2()  { return 1; }
+            virtual size_t Get3()  { return 2; }
+            virtual size_t Get4()  { return 3; }
+            virtual size_t Get5()  { return 4; }
+            virtual size_t Get6()  { return 5; }
+            virtual size_t Get7()  { return 6; }
+            virtual size_t Get8()  { return 7; }
+            virtual size_t Get9()  { return 8; }
+            virtual size_t Get10() { return 9; }
         } vt;
 
         T* object = reinterpret_cast<T*>(&vt);
 
         typedef size_t (T::*GetIndex)();
         GetIndex getIndex = reinterpret_cast<GetIndex>(function);
-        return (object->*getIndex)();
+        return (object->*getIndex)() * sizeof(uintptr_t);
     }
 }
